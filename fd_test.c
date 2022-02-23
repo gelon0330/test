bb11201e4407fbbee6add3f576974ab86d456de1
gelon0330 <61926547+gelon0330@users.noreply.github.com>
2022-02-23 13:38:35 +0800

    Update fd_test.c

--- fd_test.c
+++ fd_test.c
@@ -6,12 +6,17 @@ int getRand()
 {
         int randNum = 0;
         int fd = open("/dev/urandom", O_RDONLY);
+        static int count = 0;
+        count++;
         if(-1 == fd)
         {
                 // handle error
+                printf("error, %d\n", count);
+                //exit(1)
         }
 
         read(fd, (char *)&randNum, sizeof(int));
+        //close(fd); 
         return randNum;
 }
 
